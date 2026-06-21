f639a96d60917c9becc1271c782e3734c2828827
ISHASWAR08 <2024ca63f@sigce.edu.in>
2026-06-21 21:42:16 +0530

    completed landing page and onboarding navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+
 import { UserProvider } from "./context/UserContext";
+
 import Landing from "./pages/Landing";
 import Onboarding from "./pages/Onboarding";
 import Assessment from "./pages/Assessment";
 import Dashboard from "./pages/Dashboard";
 import Roadmap from "./pages/Roadmap";
 
-function App() {
+
+function AppRoutes() {
+
+  const navigate = useNavigate();
   return (
-    <UserProvider>
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Landing />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/assessment" element={<Assessment />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/roadmap" element={<Roadmap />} />
-
+  <Routes>
+      <Route
+        path="/"
+        element={
+          <Landing onGetStarted={() => navigate("/onboarding")}/>
+        }
+      />
+      <Route 
+        path="/onboarding" 
+        element={<Onboarding />} 
+      />
+      <Route 
+        path="/assessment" 
+        element={<Assessment />} 
+      />
+      <Route 
+        path="/dashboard" 
+        element={<Dashboard />} 
+      />
+      <Route 
+        path="/roadmap" 
+        element={<Roadmap />} 
+      />
       </Routes>
-    </BrowserRouter>
-    </UserProvider>
+
   );
 }
-
+function App() {
+ return (
+  <UserProvider>
+    <BrowserRouter>
+    <AppRoutes />
+    </BrowserRouter>
+    </UserProvider>
+    );
+  }
 export default App;
